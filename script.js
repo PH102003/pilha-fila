872e00b2092dca9be626ca0ad10c5d1a1cc4c935
Henrique Azevedo <riqueazevedo87@gmail.com>
2024-04-05 08:22:51 -0300

    add

---
+++ script.js
@@ -0,0 +1,17 @@
+let pilha_fila = [];
+
+const inputNumero = document.getElementById("inputNumero");
+const btAdd = document.getElementById("btAdd");
+const btRemover = document.getElementById("btRemover");
+const btLimpar = document.getElementById("btLimpar");
+
+//let objeto = {};
+
+const handleClickBtAdd = () => {
+    let  numero = inputNumero.value;
+    if(!numero){
+        alert("Digite um número!");
+        return;
+    }
+
+}
